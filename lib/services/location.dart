c6170bc12912f761bf3f5e71f39183aa9f0c368c
Luqman <lqman104@gmail.com>
2023-08-01 15:38:33 +0700

    refactor: change try catch position

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -5,37 +5,37 @@ class Location {
   double? _latitude;
 
   Future<void> getCurrentLocation() async {
-    try {
-      bool serviceEnabled;
-      LocationPermission permission;
+    bool serviceEnabled;
+    LocationPermission permission;
 
-      // Test if location services are enabled.
-      serviceEnabled = await Geolocator.isLocationServiceEnabled();
-      if (!serviceEnabled) {
-        // Location services are not enabled don't continue
-        // accessing the position and request users of the
-        // App to enable the location services.
-        throw 'Location services are disabled.';
-      }
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      throw 'Location services are disabled.';
+    }
 
-      permission = await Geolocator.checkPermission();
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        permission = await Geolocator.requestPermission();
-        if (permission == LocationPermission.denied) {
-          // Permissions are denied, next time you could try
-          // requesting permissions again (this is also where
-          // Android's shouldShowRequestPermissionRationale
-          // returned true. According to Android guidelines
-          // your App should show an explanatory UI now.
-          throw 'Location permissions are denied';
-        }
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        throw 'Location permissions are denied';
       }
+    }
 
-      if (permission == LocationPermission.deniedForever) {
-        // Permissions are denied forever, handle appropriately.
-        throw 'Location permissions are permanently denied, we cannot request permissions.';
-      }
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      throw 'Location permissions are permanently denied, we cannot request permissions.';
+    }
 
+    try {
       // When we reach here, permissions are granted and we can
       // continue accessing the position of the device.
       Position position = await Geolocator.getCurrentPosition(
@@ -47,7 +47,7 @@ class Location {
     }
   }
 
-  double get latitude => _latitude ?? 0.0;
+  double? get latitude => _latitude;
 
-  double get longitude => _longitude ?? 0.0;
+  double? get longitude => _longitude;
 }
